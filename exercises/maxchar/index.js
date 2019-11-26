edc4060e069424e79b26e7e63594b675fb37f334
Pat Palmerston <patpalmerston@gmail.com>
2019-11-26 12:10:43 -0800

    solution for maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,18 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let max = 0,
+        maxChar = '';
+    str.split('').forEach(char => {
+        if (str.split(char).length > max) {
+            max = str.split(char).length;
+            maxChar = char;
+        }
+    });
+    return maxChar;
+}
+// function maxChar(str) {}
+// function maxChar(str) {}
 
 module.exports = maxChar;

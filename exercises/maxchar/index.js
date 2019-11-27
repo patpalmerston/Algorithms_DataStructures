dbc2c50f89ae2d002a3b5353b932b24858175dbc
Pat Palmerston <patpalmerston@gmail.com>
2019-11-26 16:03:15 -0800

    second solution for maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,18 +5,38 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
+// function maxChar(str) {
+//     let max = 0,
+//         maxChar = '';
+//     str.split('').forEach(char => {
+//         if (str.split(char).length > max) {
+//             max = str.split(char).length;
+//             maxChar = char;
+//         }
+//     });
+//     return maxChar;
+// }
 function maxChar(str) {
-    let max = 0,
-        maxChar = '';
-    str.split('').forEach(char => {
-        if (str.split(char).length > max) {
-            max = str.split(char).length;
-            maxChar = char;
+    let obj = {};
+    let max = 0;
+    let charMax = '';
+
+    for (let char of str) {
+        if (obj[char]) {
+            obj[char]++;
+        } else {
+            obj[char] = 1;
         }
-    });
-    return maxChar;
+    }
+
+    for (let char in obj) {
+        if (obj[char] > max) {
+            max = obj[char];
+            charMax = char;
+        }
+    }
+    return charMax;
 }
-// function maxChar(str) {}
 // function maxChar(str) {}
 
 module.exports = maxChar;
